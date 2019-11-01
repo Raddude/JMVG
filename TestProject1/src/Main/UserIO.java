2cebdf78cd7acbd1167f38b2938af2b9a04ef145
viren1 <viren1@172.31.76.15>
2019-11-01 13:49:19 -0400

    Started assigning character to direction

--- TestProject1/src/Main/UserIO.java
+++ TestProject1/src/Main/UserIO.java
@@ -3,6 +3,21 @@ package Main;
 /*	CLASS DEFINITION: This class deals with the user input and output, the story, and any other text-related functions.
  */
 
+import java.util.Scanner;
 public class UserIO {
+
+	Scanner in = new Scanner(System.in);
+	
+	public static  (char leftMov, char rightMov, char frontMov, char backMov) {
+	
+		
+		leftMov = 'L';
+		rightMov = 'R';
+		frontMov = 'F';
+		backMov = 'B';
+		
+	}
+	
+	return char
 
 }
