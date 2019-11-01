ec118534d626545da3d8735412ece209ff5f8229
jjmar <jjmar@164.107.151.155>
2019-11-01 14:35:14 -0400

    Completed the toString() method of the Maze class

--- TestProject1/src/Main/Maze.java
+++ TestProject1/src/Main/Maze.java
@@ -33,14 +33,32 @@ public class Maze {
 	//Returns the maze as a string where all of the 1's are replaced with * and all of the 0's are replaced with spaces.
 	public String toString()
 	{
-		String outString = Integer.toString(maze[0].length);
+		String outputString = "";
 		
-		for (int row = 0; row < maze.length; row++)
+		//These for loops iterate through every point 
+		for (int row = 0; row < maze[0].length; row++)
 		{
+			for (int col = 0; col < maze.length; col++)
+			{
+				//If the point is a 1, add a * to outputString, otherwise add a space
+				if (maze[row][col] == 1)
+				{
+					outputString += "*";
+				}
+				
+				else
+				{
+					outputString += " ";
+				}
+			}
 			
+			//Add in a new line to outputString every time that the inner loop finishes. This has the effect of printing to the next line whenever the loop is moving to a new row.
+			outputString += "\n";
 		}
 		
-		return outString;
+		
+		
+		return outputString;
 	}
 
 }
